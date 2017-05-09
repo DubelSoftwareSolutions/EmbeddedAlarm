773a6070cff11da0a472d7e5867c7bf24c1370ae
Krzysztof Dąbek <k.narg.dabek@gmail.com>
2017-05-09 16:08:49 +0200

    Wygenerowano kod nowego projektu

--- Accelerometer.c
+++
@@ -1,8 +0,0 @@
-/*
- * Accelerometer.c
- *
- *  Created on: 11.04.2017
- *      Author: Krzysztof
- */
-
-
---
+++ AlarmZL41Project/Inc/Accelerometer.h
@@ -0,0 +1,13 @@
+/*
+ * Accelerometer.h
+ *
+ *  Created on: 11.04.2017
+ *      Author: Krzysztof
+ */
+
+#ifndef ACCELEROMETER_H_
+#define ACCELEROMETER_H_
+
+
+
+#endif /* ACCELEROMETER_H_ */
